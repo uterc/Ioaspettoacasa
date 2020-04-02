28e3b35a2242530a81463bb1b8711f9d72319791
uterc <cretu.laurentiu@gmail.com>
2020-04-03 00:14:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 let img = document.querySelector("#main-img-container");
 let img2 = document.querySelector(".main-img");
+  var int;
 function imgScroll() {
   let num = 0;
   let value = 1;
-  var int;
+
 
   if (window.innerWidth < 600) {
    int = setInterval(e => {
@@ -28,6 +29,6 @@ imgScroll();
 
 img.onclick = function(){
   clearInterval(int);
-  img2.style.marginLeft = 0 + "px";
+//   img2.style.marginLeft = 0 + "px";
   img.style.overflow = 'scroll'
 }
